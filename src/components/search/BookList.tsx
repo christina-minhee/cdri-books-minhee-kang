7e3087ad102c470319b24a614c5d861a045a2d7e
ChrisK007 <tinachristina0218@gmail.com>
2026-04-27 01:58:09 +0900

    add: empty image

--- src/components/search/BookList.tsx
+++ src/components/search/BookList.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import styled from 'styled-components';
-import type { KakaoBook } from '../../types/book';
-import BookItem from './BookItem';
+import React from "react";
+import styled from "styled-components";
+import type { KakaoBook } from "../../types/book";
+import BookItem from "./BookItem";
+import iconBook from "../../assets/images/icon_book.png";
 
-const List = styled.ul.attrs({ className: 'book-list' })`
+const List = styled.ul.attrs({ className: "book-list" })`
   margin: 0;
   padding: 0;
   list-style: none;
 `;
 
-const EmptyState = styled.div.attrs({ className: 'book-list__empty' })`
+const EmptyState = styled.div.attrs({ className: "book-list__empty" })`
   padding: var(--space-10) var(--space-4);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: var(--space-6);
   text-align: center;
   color: var(--color-text-tertiary);
   font-size: var(--font-size-base);
+`;
+
+const IconBook = styled.img`
+  width: 80px;
+  height: 80px;
 `;
 
 interface BookListProps {
@@ -25,14 +35,19 @@ interface BookListProps {
 const BookList: React.FC<BookListProps> = ({
   books,
   isLoading,
-  emptyMessage = '검색 결과가 없습니다.',
+  emptyMessage = "검색 결과가 없습니다.",
 }) => {
   if (isLoading) {
     return <EmptyState>검색 중...</EmptyState>;
   }
 
   if (books.length === 0) {
-    return <EmptyState>{emptyMessage}</EmptyState>;
+    return (
+      <EmptyState>
+        <IconBook src={iconBook} alt="empty books" />
+        {emptyMessage}
+      </EmptyState>
+    );
   }
 
   return (
